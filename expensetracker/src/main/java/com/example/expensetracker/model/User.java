99e2c0ea3cfa9e247d8d2a6e5d16b28ee8495bd0
ArmanKamal <armankamal2421@gmail.com>
2021-06-01 22:00:42 -0500

    Added ExpenseTracker MVC project

---
+++ expensetracker/src/main/java/com/example/expensetracker/model/User.java
@@ -0,0 +1,65 @@
+package com.example.expensetracker.model;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String name;
+	
+	private String email;
+	
+	
+	@OneToMany
+	private Set<Category> category;
+	
+	public User() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public Set<Category> getCategory() {
+		return category;
+	}
+	public void setCategory(Set<Category> category) {
+		this.category = category;
+	}
+	
+	public User(Long id, String name, String email, Set<Category> category) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.category = category;
+	}
+}
